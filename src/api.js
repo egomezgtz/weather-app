03fca63519d8c7473494e19c5d92bdebbc687200
Emmanuel Gomez <89927027+gomezgtzem@users.noreply.github.com>
2022-09-07 16:05:51 -0500

    weather icons
    Missing weather icons

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "8f97ed61f1mshdfd9177c52587d7p1c035fjsn5844c17b57de",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "388aa732731ba2a28b4f1cd4e3baf9a4";
