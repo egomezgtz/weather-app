f3bf0a6c46fcff6700b59f9dc24e2149121ca6a5
Emmanuel Gomez <89927027+gomezgtzem@users.noreply.github.com>
2022-09-13 15:06:11 -0500

    Updates

---
+++ precache-manifest.4bdff9fd63a56260d7f55a3f7c6429c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb1c60a6efc87bb81721",
+    "url": "/weather-app/static/css/main.52a83b04.chunk.css"
+  },
+  {
+    "revision": "fb1c60a6efc87bb81721",
+    "url": "/weather-app/static/js/main.fb1c60a6.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/weather-app/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "375847d92294722d7035",
+    "url": "/weather-app/static/js/2.375847d9.chunk.js"
+  },
+  {
+    "revision": "0aab8067a2816e67105b",
+    "url": "/weather-app/static/js/runtime~main.0aab8067.js"
+  },
+  {
+    "revision": "32085d3c20702fe97c28d5b47555b638",
+    "url": "/weather-app/index.html"
+  }
+];
